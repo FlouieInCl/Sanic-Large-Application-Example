f24153243782a6ad109b39d6c0498c01f6f6f97d
JoMingyu <mingyu.planb@gmail.com>
2018-12-12 21:01:58 +0900

    Flask - Sanic, flask_app -> sanic_app
    attribute나 import 관련 에러는 여전히 있음.

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,50 @@
-from flask import Flask
+from sanic import Sanic
 from werkzeug.exceptions import HTTPException
 
 from app.misc.log import log
 
 
-def register_extensions(flask_app: Flask):
+def register_extensions(sanic_app: Sanic):
     from app import extensions
 
-    extensions.cors.init_app(flask_app)
+    extensions.cors.init_app(sanic_app)
 
 
-def register_views(flask_app: Flask):
+def register_views(sanic_app: Sanic):
     from app.views import route
 
-    handle_exception_func = flask_app.handle_exception
-    handle_user_exception_func = flask_app.handle_user_exception
+    handle_exception_func = sanic_app.handle_exception
+    handle_user_exception_func = sanic_app.handle_user_exception
     # register_blueprint 시 defer되었던 함수들이 호출되며, flask-restful.Api._init_app()이 호출되는데
     # 해당 메소드가 app 객체의 에러 핸들러를 오버라이딩해서, 별도로 적용한 handler의 HTTPException 관련 로직이 동작하지 않음
     # 따라서 두 함수를 임시 저장해 두고, register_blueprint 이후 함수를 재할당하도록 함
 
-    route(flask_app)
+    route(sanic_app)
 
-    flask_app.handle_exception = handle_exception_func
-    flask_app.handle_user_exception = handle_user_exception_func
+    sanic_app.handle_exception = handle_exception_func
+    sanic_app.handle_user_exception = handle_user_exception_func
 
 
-def register_hooks(flask_app: Flask):
+def register_hooks(sanic_app: Sanic):
     from app.hooks.error import broad_exception_error_handler, http_exception_handler
     from app.hooks.request_context import after_request
 
-    flask_app.after_request(after_request)
-    flask_app.register_error_handler(HTTPException, http_exception_handler)
-    flask_app.register_error_handler(Exception, broad_exception_error_handler)
+    sanic_app.after_request(after_request)
+    sanic_app.register_error_handler(HTTPException, http_exception_handler)
+    sanic_app.register_error_handler(Exception, broad_exception_error_handler)
 
 
-def create_app(*config_cls) -> Flask:
-    log(message='Flask application initialized with {}'.format(', '.join([config.__name__ for config in config_cls])),
+def create_app(*config_cls) -> Sanic:
+    log(message='Sanic application initialized with {}'.format(', '.join([config.__name__ for config in config_cls])),
         keyword='INFO')
             
-    flask_app = Flask(__name__)
+    sanic_app = Sanic(__name__)
 
     for config in config_cls:
-        flask_app.config.from_object(config)
+        sanic_app.config.from_object(config)
 
-    register_extensions(flask_app)
-    register_views(flask_app)
-    register_hooks(flask_app)
+    register_extensions(sanic_app)
+    register_views(sanic_app)
+    register_hooks(sanic_app)
 
-    return flask_app
+    return sanic_app
