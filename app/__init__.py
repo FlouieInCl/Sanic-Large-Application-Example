97d7dce418ef554f873546d842c6cbe71a1c37cf
JoMingyu <mingyu.planb@gmail.com>
2018-12-12 21:06:39 +0900

    app\__init__.py sanic 포팅

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from sanic import Sanic
-from werkzeug.exceptions import HTTPException
 
 from app.misc.log import log
 
@@ -13,31 +12,21 @@ def register_extensions(sanic_app: Sanic):
 def register_views(sanic_app: Sanic):
     from app.views import route
 
-    handle_exception_func = sanic_app.handle_exception
-    handle_user_exception_func = sanic_app.handle_user_exception
-    # register_blueprint 시 defer되었던 함수들이 호출되며, flask-restful.Api._init_app()이 호출되는데
-    # 해당 메소드가 app 객체의 에러 핸들러를 오버라이딩해서, 별도로 적용한 handler의 HTTPException 관련 로직이 동작하지 않음
-    # 따라서 두 함수를 임시 저장해 두고, register_blueprint 이후 함수를 재할당하도록 함
-
     route(sanic_app)
-
-    sanic_app.handle_exception = handle_exception_func
-    sanic_app.handle_user_exception = handle_user_exception_func
 
 
 def register_hooks(sanic_app: Sanic):
-    from app.hooks.error import broad_exception_error_handler, http_exception_handler
+    from app.hooks.error import broad_exception_handler
     from app.hooks.request_context import after_request
 
-    sanic_app.after_request(after_request)
-    sanic_app.register_error_handler(HTTPException, http_exception_handler)
-    sanic_app.register_error_handler(Exception, broad_exception_error_handler)
+    sanic_app.register_middleware(after_request, 'response')
+    sanic_app.error_handler.add(Exception, broad_exception_handler)
 
 
 def create_app(*config_cls) -> Sanic:
     log(message='Sanic application initialized with {}'.format(', '.join([config.__name__ for config in config_cls])),
         keyword='INFO')
-            
+
     sanic_app = Sanic(__name__)
 
     for config in config_cls:
